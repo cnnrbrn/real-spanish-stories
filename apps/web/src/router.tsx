aa5ce4823ec0be7f83ccfe8245a92c7adf6418f3
Connor O'Brien <cnnrbrn@proton.me>
2026-04-15 16:37:09 -0300

    Remove qs dependency (level filter disabled)

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,5 +1,4 @@
 import { createRouter } from '@tanstack/react-router'
-import qs from 'qs'
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
 import * as TanstackQuery from './integrations/tanstack-query/root-provider'
 
@@ -15,14 +14,6 @@ export const getRouter = () => {
     routeTree,
     context: {
       ...rqContext,
-    },
-    parseSearch: (searchStr) =>
-      qs.parse(searchStr.startsWith('?') ? searchStr.slice(1) : searchStr, {
-        comma: true,
-      }),
-    stringifySearch: (search) => {
-      const result = qs.stringify(search, { arrayFormat: 'comma', encode: false })
-      return result ? `?${result}` : ''
     },
     defaultPreload: 'intent',
     defaultNotFoundComponent: () => <NotFound />,
